0308f0d3897c18b9914e7ef442296c2e497ed5cf
Renaud Gaujoux <renaud@tx.technion.ac.il>
2017-04-10 13:54:40 +0000

    version 1.6.6

--- inst/doc/doRNG-unitTests.R
+++ inst/doc/doRNG-unitTests.R
@@ -1,9 +1,7 @@
-
 ## ----setup, include=FALSE------------------------------------------------
 pkg <- 'doRNG'
 require( pkg, character.only=TRUE )
 prettyVersion <- packageDescription(pkg)$Version
 prettyDate <- format(Sys.Date(), '%B %e, %Y')
 authors <- packageDescription(pkg)$Author
-
 
--- inst/doc/doRNG.R
+++ inst/doc/doRNG.R
@@ -1,21 +1,17 @@
-
 ## ----pkgmaker_preamble, echo=FALSE, results='asis'-----------------------
 pkgmaker::latex_preamble()
-
 
 ## ----bibliofile, echo=FALSE, results='asis'------------------------------
 pkgmaker::latex_bibliography('doRNG')
 
-
-## ----init, include = FALSE-----------------------------------------------
+## ----init, include = FALSE--------------------------------------------------------------
 options(width=90)
 library(pkgmaker)
 library(knitr)
 opts_chunk$set(size = "footnotesize")
 knit_hooks$set(try = pkgmaker::hook_try)
 
-
-## ----foreach-------------------------------------------------------------
+## ----foreach----------------------------------------------------------------------------
 # load and register parallel backend for multicore computations
 library(doParallel)
 cl <- makeCluster(2)
@@ -27,8 +23,7 @@ x <- foreach(i=1:5) %dopar% {
 }
 unlist(x)
 
-
-## ----dopar, tidy=FALSE---------------------------------------------------
+## ----dopar, tidy=FALSE------------------------------------------------------------------
 # with standard %dopar%: foreach loops are not reproducible
 set.seed(123)
 res <- foreach(i=1:5) %dopar% { runif(3) }
@@ -36,8 +31,7 @@ set.seed(123)
 res2 <- foreach(i=1:5) %dopar% { runif(3) }
 identical(res, res2)
 
-
-## ----dorng, tidy=FALSE---------------------------------------------------
+## ----dorng, tidy=FALSE------------------------------------------------------------------
 
 # load the doRNG package
 library(doRNG)
@@ -49,23 +43,19 @@ set.seed(123)
 res2 <- foreach(i=1:5) %dorng% { runif(3) }
 identical(res, res2)
 
-
-## ----attr----------------------------------------------------------------
+## ----attr-------------------------------------------------------------------------------
 attr(res, 'rng')
 
-
-## ----nextRNGstream-------------------------------------------------------
+## ----nextRNGstream----------------------------------------------------------------------
 nextRNGStream(c(407L, 1:6))
 
-
-## ----options_single------------------------------------------------------
+## ----options_single---------------------------------------------------------------------
 # use a single numeric as a seed
 s <- foreach(i=1:5, .options.RNG=123) %dorng% { runif(3) }
 s2 <- foreach(i=1:5, .options.RNG=123) %dorng% { runif(3) }
 identical(s, s2)
 
-
-## ----options_single_normalkind-------------------------------------------
+## ----options_single_normalkind----------------------------------------------------------
 ## Pass the Normal RNG kind to use within the loop
 # results are identical if not using the Normal kind in the loop
 optsN <- list(123, normal.kind="Ahrens")
@@ -79,21 +69,18 @@ resN2 <- foreach(i=1:5, .options.RNG=optsN) %dorng% { rnorm(3) }
 identical(resN[1:5], resN1[1:5])
 identical(resN1[1:5], resN2[1:5])
 
-
-## ----options_6length-----------------------------------------------------
+## ----options_6length--------------------------------------------------------------------
 # use a 6-length numeric
 s <- foreach(i=1:5, .options.RNG=1:6) %dorng% { runif(3) }
 attr(s, 'rng')[1:3]
 
-
-## ----options_7length-----------------------------------------------------
+## ----options_7length--------------------------------------------------------------------
 # use a 7-length numeric, used as first value for .Random.seed
 seed <- attr(res, 'rng')[[2]]
 s <- foreach(i=1:5, .options.RNG=seed) %dorng% { runif(3) }
 identical(s[1:4], res[2:5])
 
-
-## ----options_list--------------------------------------------------------
+## ----options_list-----------------------------------------------------------------------
 # reproduce previous %dorng% loop
 s <- foreach(i=1:5, .options.RNG=res) %dorng% { runif(3) }
 identical(s, res)
@@ -108,8 +95,7 @@ seedL <- lapply(seq(ncol(seedM)), function(i) seedM[,i])
 sL <- foreach(i=1:5, .options.RNG=seedL) %dorng% { runif(3) }
 identical(sL, sM)
 
-
-## ----set_seed_diff-------------------------------------------------------
+## ----set_seed_diff----------------------------------------------------------------------
 # default RNG kind
 RNGkind('default')
 def <- foreach(i=1:5, .options.RNG=123) %dorng% { runif(3) }
@@ -122,8 +108,7 @@ identical(def, mars)
 # revert to default RNG kind
 RNGkind('default')
 
-
-## ----schedule, tidy=FALSE------------------------------------------------
+## ----schedule, tidy=FALSE---------------------------------------------------------------
 # define a stochastic task to perform
 task <- function() c(pid=Sys.getpid(), val=runif(1))
 
@@ -160,8 +145,7 @@ pid
 # results are identical
 identical(res_seq[,2], res_2workers[,2]) && identical(res_2workers[,2], res_3workers[,2])
 
-
-## ----registerDoRNG-------------------------------------------------------
+## ----registerDoRNG----------------------------------------------------------------------
 
 set.seed(123)
 res <- foreach(i=1:5) %dorng% { runif(3) }
@@ -171,8 +155,7 @@ res_dopar <- foreach(i=1:5) %dopar% { runif(3) }
 identical(res_dopar, res)
 attr(res_dopar, 'rng')
 
-
-## ----multiple, tidy=FALSE------------------------------------------------
+## ----multiple, tidy=FALSE---------------------------------------------------------------
 set.seed(456)
 s1 <- foreach(i=1:5) %dorng% { runif(3) }
 s2 <- foreach(i=1:5) %dorng% { runif(3) }
@@ -191,16 +174,14 @@ r1 <- foreach(i=1:5) %dopar% { runif(3) }
 r2 <- foreach(i=1:5) %dopar% { runif(3) }
 identical(r1, s1) && identical(r2, s2)
 
-
-## ----nested_error, error = TRUE, try = TRUE------------------------------
+## ----nested_error, error = TRUE, try = TRUE---------------------------------------------
 # nested loop
 try( foreach(i=1:10) %:% foreach(j=1:i) %dorng% { rnorm(1) } )
 
 # conditional loop
 try( foreach(i=1:10) %:% when(i %% 2 == 0) %dorng% { rnorm(1) } )
 
-
-## ----nested--------------------------------------------------------------
+## ----nested-----------------------------------------------------------------------------
 
 # doRNG must not be registered
 registerDoParallel(cl)
@@ -232,8 +213,7 @@ res2 <- foreach(i=1:n) %:% foreach(j=1:p) %do%{
 
 stopifnot( identical(res, res2) )
 
-
-## ----nested_unequal------------------------------------------------------
+## ----nested_unequal---------------------------------------------------------------------
 # generate sequence of seeds of length the number of computations
 n <- 10
 rng <- RNGseq( n * (n+1) / 2, 1234)
@@ -261,8 +241,7 @@ res2 <- foreach(i=1:n) %:% foreach(j=1:i) %do%{
 
 stopifnot( identical(res, res2) )
 
-
-## ----conditional---------------------------------------------------------
+## ----conditional------------------------------------------------------------------------
 
 # un-conditional single loop
 resAll <- foreach(i=1:n, .options.RNG=1234) %dorng%{
@@ -309,8 +288,7 @@ stopifnot( identical(resAll[seq(1,n,by=2)], resOdd) )
 stopifnot( identical(resAll[c(1,2,n-1,n)], resFL) )
 
 
-
-## ----nested_conditional--------------------------------------------------
+## ----nested_conditional-----------------------------------------------------------------
 # generate sequence of seeds of length the number of computations
 n <- 10
 rng <- RNGseq( n * (n+1) / 2, 1234)
@@ -338,8 +316,7 @@ resAll <- foreach(i=1:n) %:% foreach(j=1:i) %do%{
 
 stopifnot( identical(resAll[seq(2,n,by=2)], res) )
 
-
-## ----perf, cache=TRUE----------------------------------------------------
+## ----perf, cache=TRUE-------------------------------------------------------------------
 # load rbenchmark
 library(rbenchmark)
 
@@ -358,24 +335,18 @@ cmp <- benchmark(rPar(10), rRNG(10)
 # order by increasing elapsed time
 cmp[order(cmp$elapsed), ]
 
-
-## ----doRNGversion--------------------------------------------------------
+## ----doRNGversion-----------------------------------------------------------------------
 doRNGversion('1.3')
 
-
-## ----doRNGversion_revert-------------------------------------------------
+## ----doRNGversion_revert----------------------------------------------------------------
 doRNGversion(NULL)
 
-
-## ----news, echo=FALSE, results='asis'------------------------------------
+## ----news, echo=FALSE, results='asis'---------------------------------------------------
 cat(paste(readLines(system.file('NEWS', package='doRNG')), collapse="\n"))
 
-
-## ----stopCluster---------------------------------------------------------
+## ----stopCluster------------------------------------------------------------------------
 stopCluster(cl)
 
-
-## ----session_info, echo=FALSE, comment=NA--------------------------------
+## ----session_info, echo=FALSE, comment=NA-----------------------------------------------
 sessionInfo()
-
 
